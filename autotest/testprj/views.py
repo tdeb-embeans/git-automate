36bdf699f34c2d8534f5c887e97b6722f9b0acd7
incise <tanmoy.deb@incise.in>
2017-01-09 18:37:47 +0530

    bootstrap GUI is added.

--- autotest/testprj/views.py
+++ autotest/testprj/views.py
@@ -4,7 +4,22 @@ from .models import TestCase
 
 def index(request):
     test_cases_date_time_wise_list = TestCase.objects.all()
-    context = {'test_cases_date_time_wise_list':test_cases_date_time_wise_list}
+    countPassCases = (TestCase.objects.filter(result = 'pass')).__len__()
+    print ("TD:countPassCases", countPassCases)
+    countFailCases = (TestCase.objects.filter(result = 'fail')).__len__()
+ 
+    countNECases = (TestCase.objects.filter(result = 'NE')).__len__()
+
+    countTotalCases = countPassCases + countFailCases + countNECases
+    context = {'countPassCases': countPassCases,
+	       'countFailCases':countFailCases,
+		'countNECases':countNECases,
+		'countTotalCases':countTotalCases}
+    
+
     return render(request, 'testprj/index.html', context)# Create your views here.
+
+
+
 
 
